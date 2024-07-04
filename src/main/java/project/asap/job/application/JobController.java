007e8d7d18f4ea4545b8befeb79974cadf6c30a6
Fauzan Saef <fauzansaef@gmail.com>
2024-07-04 16:20:39 +0700

    feat : add controller

--- src/main/java/project/asap/job/application/JobController.java
+++ src/main/java/project/asap/job/application/JobController.java
@@ -1,4 +1,24 @@
 package project.asap.job.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/job/")
 public class JobController {
+    private final JobService jobService;
+
+    @Autowired
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+
+    @GetMapping("load-excel")
+    public void loadExcel() {
+        jobService.runJob();
+    }
+
 }
